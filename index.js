cf075a7d89952f998983c24934203462413af125
frontendella <ryukhinaella@gmail.com>
2023-03-28 21:36:37 -0700

    Set up app skeleton

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+const addButtonEl = document.getElementById('add-button')
+const inputFieldEl = document.getElementById('input-field')
+
+addButtonEl.addEventListener("click", ()=>{
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
